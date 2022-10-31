b990407914ad01f5ef37d800387ed2c86fef1367
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-10-31 13:17:12 +0530

    [Afzal] Add: SnakeandLadder Folder Structure

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SnakeAndLadder Problem Statement");
+        }
+    }
+}
